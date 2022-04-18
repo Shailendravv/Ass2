70542fec8e8fdb420ce2f66ced64f972d9407353
Shailendra <shailendramiet2@gmail.com>
2022-04-18 21:30:20 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+// second argument
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [list, setList] = useState();
+
+  const handleRemove = (id) => {
+    // console.log(id);
+    const newList = users.filter((item) => item.id !== id);
+    console.log(newList);
+    setList(newList);
+  };
+
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users);
+    // console.log(users);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>Json users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, userId, title, body } = user;
+          return (
+            <li key={id}>
+              <div>
+                <h4>UserId: {userId}</h4>
+                <h4>Id: {id}</h4>
+                <h4>Title: {title}</h4>
+                <p>Body: {body}</p>
+                <button onClick={() => handleRemove(user.id)}>Remove</button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
-}
+};
 
 export default App;
